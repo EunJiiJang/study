fc94ed6920b58b4d855e75e627b85bd275ba1bc7
eunji-jang <gkgksmf1@naver.com>
2021-08-30 23:54:39 +0900

    tree2_suc

--- data Structure/tree02.py
+++ data Structure/tree02.py
@@ -1,6 +1,3 @@
-from _typeshed import Self
-
-
 class Node:
     def __init__(self,value):
         self.value = value
@@ -37,8 +34,8 @@ class NoneMgmt:
                 self.current_node = self.current_node.left
             else:
                 self.current_node = self.current_node.right
-
         return False
+
     def delete(self,value):
         searched = False
         self.current_node =self.head
@@ -57,26 +54,31 @@ class NoneMgmt:
         if searched == False:
             return False
 
-## self.current_node 가 삭제할 Node, self.parent는 삭제할 Node의 Parent Node인 상태
-        if self.current_node.left == None and self.current_node.right == None:
+        ## self.current_node 가 삭제할 Node, self.parent는 삭제할 Node의 Parent Node인 상태
+        # case1
+        if  self.current_node.left == None and self.current_node.right == None:
             if value < self.parent.value:
                 self.parent.left = None
             else:
-                self.parent.right =None
-            del self.current_node
-#삭제할 Node가 Child Node를 한 개 가지고 있을 경우
-        if self.current_node.left != None and self.current_node.right ==None:
+                self.parent.right = None
+            
+        #삭제할 Node가 Child Node를 한 개 가지고 있을 경우
+        # case2
+        elif self.current_node.left != None and self.current_node.right == None:
             if value < self.parent.value:
                 self.parent.left = self.current_node.left
             else:
-                self.parent.right =self.current_node.left
+                self.parent.right = self.current_node.left
         elif self.current_node.left == None and self.current_node.right != None:
             if value < self.parent.value:
                 self.parent.left = self.current_node.right
             else:
-                self.parent.right = self.current_node.right
-#삭제할 Node가 Child Node를 두 개 가지고 있을 경우 (삭제할 Node가 Parent Node 왼쪽에 있을 때)
-        if self.current_node.left != None and self.current_node.right != None:#case3
+                self.parent.right = self.current_node.right        
+        
+        #삭제할 Node가 Child Node를 두 개 가지고 있을 경우 (삭제할 Node가 Parent Node 왼쪽에 있을 때)
+        #case3
+        elif self.current_node.left != None and self.current_node.right != None:#case3
+            #case3-1
             if value < self.parent.value:#case3-1
                 self.change_node = self.current_node.right
                 self.change_node_parent = self.current_node.right
@@ -90,8 +92,23 @@ class NoneMgmt:
                 self.parent.left = self.change_node
                 self.change_node.right = self.current_node.right
                 self.change_node.left = self.change_node.left
-#삭제할 Node가 Child Node를 두 개 가지고 있을 경우 (삭제할 Node가 Parent Node 오른쪽에 있을 때)
-                    
+            #삭제할 Node가 Child Node를 두 개 가지고 있을 경우 (삭제할 Node가 Parent Node 오른쪽에 있을 때)
+            # case 3-2
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left =None
+                self.parent.right = self.change_node
+                self.change_node.left = self.current_node.left
+                self.change_node.right = self.current_node.right            
+            
+        return True
 
 head = Node(1)
 bst = NoneMgmt(head)
