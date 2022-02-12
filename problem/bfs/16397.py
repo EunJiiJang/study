cdf5ac006bd3b4893bf4714b6abb13a112693df3
eunji-jang <dmswlquf@naver.com>
2022-02-12 23:25:01 +0900

    solved

--- problem/bfs/16397.py
+++ problem/bfs/16397.py
@@ -1,20 +1,29 @@
 from collections import deque
-n,t,g = map(int,input().split())
-visit = [0 for i in range(100000)]
-q = deque()
-q.append([n,0])
-while q:
-    p,d = q[0][0],q[0][1]
-    if d > t:
-        break
-    if p == g:
-        
-    q.popleft()
-    visit[p] = 1
-    if p - 1>= 0 and visit[p-1]==0:
-        q.append([p-1,d+1])
-    if p+1<= 100000 and visit[p+1] == 0:
-        q.append([p+1,d+1])
-    if p*2 <=100000 and visit[p*2]==0:
-        q.append([p*2,d+1])
-print(q[0][1])
+
+def bfs():
+    q = deque()
+    q.append(n)
+    visit[n] = 0
+    while q:
+        x = q.popleft()
+        if visit[x] > t:
+            break
+        if x == g:
+            print(visit[x])
+            return
+        dx = [(x+1), (x*2)]
+        for i in range(2):
+            nx = dx[i]
+            if nx > 99999:
+                continue
+            if i and x:
+                nx -= 10**(len(str(nx))-1)
+            if visit[nx] == -1:
+                q.append(nx)
+                visit[nx] = visit[x]+1
+    print("ANG")
+
+n, t, g = map(int, input().split())
+visit = [-1]*100000
+bfs()
+
