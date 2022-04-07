b1157f2bbac3ae13a72adfa5e1db245d915d2100
eunji-jang <dmswlquf@naver.com>
2022-04-07 16:49:20 +0900

    solved_err

---
+++ problem/dp/1213.py
@@ -0,0 +1,23 @@
+from collections import Counter
+word = list(map(str,input().strip()))
+word.sort
+check = Counter(word)
+cen = ""
+cnt = 0
+for i in check:
+    if check[i]%2 != 0 :
+        cnt += 1
+        cen += i
+        word.remove(i)
+
+    if cnt > 1:
+        break
+
+if cnt > 1:
+        print("I'm Sorry Hansoo")
+
+else:
+    res = ""
+    for i in range(0,len(word),2):
+        res += word[i]
+    print(res+cen+res[::-1])
