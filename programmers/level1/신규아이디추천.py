fbed46732b9ddabe82c5feee0dbd4c6304e037cf
eunji-jang <dmswlquf@naver.com>
2022-05-04 16:42:22 +0900

    solved

---
+++ programmers/level1/신규아이디추천.py
@@ -0,0 +1,12 @@
+import re
+def solution(new_id):
+    answer = re.sub('[^0-9a-z_.\-]+','',new_id.lower())
+    answer = re.sub('\.\.+','.',answer)
+    answer = answer.strip('.')
+    if answer == '': answer+='a'
+    answer = answer[:15].rstrip('.')
+    answer += answer[-1]*(3-len(answer))
+
+    return answer
+
+print(solution("...!@BaT#*..y.abcdefghijklm"))
