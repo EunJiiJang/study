0b02dc57cf1325b874a73be8303841c9849e0eb1
jangeunji <eunji.jang@revisiononline.com.au>
2025-11-07 17:18:37 +0000

    feat leetcode

---
+++ reetcode/dp/70_climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+
+        p1 = 3
+        p2 = 2
+        cur = 0
+        for _ in range(3,n):
+            cur = p1+p2
+            p2 = p1
+            p1 = cur
+
+        return cur
